37144b9a78a84a93638dff0c5bbb9a2cef0f5e46
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-03-28 22:25:09 -0400

    [google_cloud_c/topic] Implement topic API for `get_topic`, `create_topic`, `delete_topic`

---
+++ google_cloud_c/topic/topic.c
@@ -0,0 +1,151 @@
+#include <parson.h>
+
+#include <json_common.h>
+
+#include <google_cloud_c/client/cloud_auth.h>
+#include <google_cloud_c/topic/topic.h>
+
+const struct Topic EMPTY_TOPIC = {NULL,  NULL, NULL, NULL, ENCODING_UNSPECIFIED,
+                                  false, NULL};
+
+/* Gets the configuration of a topic.
+ * GET https://pubsub.googleapis.com/v1/{topic}
+ * https://cloud.google.com/pubsub/docs/reference/rest/v1/projects.topics/get
+ * */
+struct OptionalTopic get_topic(const char *topic_id) {
+  char *path;
+  asprintf(&path, "/v1/%s", topic_id);
+
+  {
+    struct ServerResponse response = gcloud_pubsub_get(NULL, path, NULL);
+    if (response.status_code == 200 && strlen(response.body) > 0) {
+      struct OptionalTopic optionalTopic = {
+          true, topic_from_json(json_value_get_object(
+                    if_error_exit(json_parse_string(response.body), false)))};
+      return optionalTopic;
+    } else {
+      const struct OptionalTopic optionalTopic = {false, EMPTY_TOPIC};
+      return optionalTopic;
+    }
+  }
+}
+
+/* Creates the given topic with the given name.
+ * PUT https://pubsub.googleapis.com/v1/{name}
+ * https://cloud.google.com/pubsub/docs/reference/rest/v1/projects.topics/create
+ * */
+struct OptionalTopic create_topic(const char *topic_id, struct Topic topic) {
+  char *path;
+  asprintf(&path, "/v1/%s", topic_id);
+
+  {
+    struct ServerResponse response =
+        gcloud_pubsub_put(NULL, path, topic_to_json(topic), NULL);
+    if (response.status_code == 200 && strlen(response.body) > 0) {
+      struct OptionalTopic optionalTopic = {
+          true, topic_from_json(json_value_get_object(
+                    if_error_exit(json_parse_string(response.body), false)))};
+      return optionalTopic;
+    } else {
+      const struct OptionalTopic optionalTopic = {false, EMPTY_TOPIC};
+      return optionalTopic;
+    }
+  }
+}
+
+/* Deletes the topic with the given name.
+ * DELETE https://pubsub.googleapis.com/v1/{topic}
+ * https://cloud.google.com/pubsub/docs/reference/rest/v1/projects.topics/delete
+ * */
+bool delete_topic(const char *topic_id) {
+  char *path;
+  asprintf(&path, "/v1/%s", topic_id);
+
+  {
+    struct ServerResponse response =
+        gcloud_pubsub_delete(NULL, path, NULL, NULL);
+    /* free(path) */
+    return response.status_code == 200;
+  }
+}
+
+/* Utility functions */
+
+enum Encoding str_to_Encoding(const char *encoding) {
+  if (strcmp(encoding, "JSON") == 0)
+    return JSON;
+  else if (strcmp(encoding, "BINARY") == 0)
+    return BINARY;
+  else
+    return ENCODING_UNSPECIFIED;
+}
+
+const char *Encoding_to_str(enum Encoding encoding) {
+  switch (encoding) {
+  case JSON:
+    return "JSON";
+  case BINARY:
+    return "BINARY";
+  case ENCODING_UNSPECIFIED:
+  default:
+    return "ENCODING_UNSPECIFIED";
+  }
+}
+
+struct Topic topic_from_json(const JSON_Object *jsonObject) {
+  struct Topic topic;
+
+  topic.name = json_object_get_string(jsonObject, "name");
+  {
+    const JSON_Object *jsonMessageStoragePolicy =
+        json_object_get_object(jsonObject, "messageStoragePolicy");
+    struct MessageStoragePolicy messageStoragePolicy;
+    messageStoragePolicy.allowedPersistenceRegions =
+        (const char **)json_object_get_array(jsonMessageStoragePolicy,
+                                             "allowedPersistenceRegions");
+    topic.messageStoragePolicy = messageStoragePolicy;
+  }
+  topic.kmsKeyName = json_object_get_string(jsonObject, "kmsKeyName");
+  {
+    const JSON_Object *jsonSchemaSettings =
+        json_object_get_object(jsonObject, "SchemaSettings");
+    struct SchemaSettings schemaSettings;
+    schemaSettings.schema = json_object_get_string(jsonObject, "schema");
+    schemaSettings.encoding =
+        str_to_Encoding(json_object_get_string(jsonSchemaSettings, "encoding"));
+    topic.schemaSettings = schemaSettings;
+  }
+  topic.satisfiesPzs = (bool)json_object_get_boolean(jsonObject, "kmsKeyName");
+  topic.messageRetentionDuration =
+      json_object_get_string(jsonObject, "messageRetentionDuration");
+
+  return topic;
+}
+
+const char *SchemaSettings_to_json(struct SchemaSettings schemaSettings) {
+  char *s;
+  asprintf(&s,
+           "{"
+           "  \"schema\": \"%s\","
+           "  \"encoding\": \"%s\""
+           "}",
+           schemaSettings.schema, Encoding_to_str(schemaSettings.encoding));
+  return s;
+}
+
+const char *topic_to_json(struct Topic topic) {
+  char *s;
+  asprintf(&s,
+           "{"
+           "  \"name\": \"%s\","
+           "  \"kmsKeyName\": \"%s\","
+           "  \"satisfiesPzs\": %ld,"
+           "  \"messageRetentionDuration\": \"%s\","
+           "  \"schemaSettings\": %s"
+           "}",
+           /* TODO: MessageStoragePolicy */
+           topic.name, topic.kmsKeyName, topic.satisfiesPzs,
+           topic.messageRetentionDuration,
+           SchemaSettings_to_json(topic.schemaSettings));
+  return s;
+}
---
+++ google_cloud_c/topic/topic.h
@@ -0,0 +1,86 @@
+#ifndef GOOGLE_CLOUD_C_TOPIC_H
+#define GOOGLE_CLOUD_C_TOPIC_H
+
+#ifdef __cplusplus
+#include <cstdbool>
+extern "C" {
+#elif __STDC_VERSION__ >= 199901L
+#include <stdbool.h>
+#else
+#include <google_cloud_c/common/google_cloud_c_stdbool.h>
+#endif /* __cplusplus */
+
+#include <google_cloud_c/topic/google_cloud_c_topic_export.h>
+
+/*
+ * https://cloud.google.com/pubsub/docs/reference/rest/v1/projects.topics#MessageStoragePolicy
+ */
+struct MessageStoragePolicy {
+  const char **allowedPersistenceRegions;
+};
+
+/*
+ * https://cloud.google.com/pubsub/docs/reference/rest/v1/projects.topics#SchemaSettings
+ */
+enum Encoding { ENCODING_UNSPECIFIED, JSON, BINARY };
+
+extern GOOGLE_CLOUD_C_TOPIC_EXPORT enum Encoding str_to_Encoding(const char *);
+
+extern GOOGLE_CLOUD_C_TOPIC_EXPORT const char *Encoding_to_str(enum Encoding);
+
+struct SchemaSettings {
+  const char *schema;
+  enum Encoding encoding;
+};
+
+/*
+ * https://cloud.google.com/pubsub/docs/reference/rest/v1/projects.topics#Topic
+ */
+struct Topic {
+  const char *name;
+  /*"labels": {
+    string: string,
+      ...
+  },*/
+  struct MessageStoragePolicy messageStoragePolicy;
+  const char *kmsKeyName;
+  struct SchemaSettings schemaSettings;
+  bool satisfiesPzs;
+  const char *messageRetentionDuration;
+};
+
+struct OptionalTopic {
+  bool set;
+  struct Topic topic;
+};
+
+extern GOOGLE_CLOUD_C_TOPIC_EXPORT const struct Topic EMPTY_TOPIC;
+
+/* Gets the configuration of a topic.
+ * https://cloud.google.com/pubsub/docs/reference/rest/v1/projects.topics/get
+ */
+extern GOOGLE_CLOUD_C_TOPIC_EXPORT struct OptionalTopic get_topic(const char *);
+
+/* Creates the given topic with the given name.
+ * https://cloud.google.com/pubsub/docs/reference/rest/v1/projects.topics/create
+ */
+extern GOOGLE_CLOUD_C_TOPIC_EXPORT struct OptionalTopic
+create_topic(const char *, struct Topic);
+
+/* Deletes the topic with the given name.
+ * https://cloud.google.com/pubsub/docs/reference/rest/v1/projects.topics/delete
+ * */
+extern GOOGLE_CLOUD_C_TOPIC_EXPORT bool delete_topic(const char *topic_id);
+
+/* utility functions */
+
+extern GOOGLE_CLOUD_C_TOPIC_EXPORT struct Topic
+topic_from_json(const JSON_Object *);
+
+extern GOOGLE_CLOUD_C_TOPIC_EXPORT const char *topic_to_json(struct Topic);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* !GOOGLE_CLOUD_C_TOPIC_H */
